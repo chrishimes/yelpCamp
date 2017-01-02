24a306c1f0eaa2b6f821f860728762c34c8efc7f
Chris Himes <chris@chrishimes.com>
2017-01-01 18:39:32 -0700

    finished add items

--- 6-ToDoList/assets/js/todos.js
+++ 6-ToDoList/assets/js/todos.js
@@ -1,13 +1,26 @@
 //Check off Specific Todos by Clicking
-$("li").click(function(){
+//This will use to drill from the top down and 
+//allow it to work on newly created items
+$("ul").on("click", "li",  function(){
 	$(this).toggleClass("completed");
 });
  console.log("inside js file");
 
 //Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		//remove value in the text toDos
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> "+ todoText +"</li>");
+	}
 });
