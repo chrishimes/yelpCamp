f1ff19caa3f73902cb9df55287211e248ab300a1
Chris Himes <chris@chrishimes.com>
2017-01-01 20:59:31 -0700

    to do list done

--- 6-ToDoList/assets/js/todos.js
+++ 6-ToDoList/assets/js/todos.js
@@ -21,6 +21,10 @@ $("input[type='text']").keypress(function(event){
 		//remove value in the text toDos
 		$(this).val("");
 		//create a new li and add to ul
-		$("ul").append("<li><span>X</span> "+ todoText +"</li>");
+		$("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> "+ todoText +"</li>");
 	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
